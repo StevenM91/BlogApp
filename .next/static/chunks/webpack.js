d81bf416626fa0326b6e87280e1f6813b82f6631
Steven Maitre <steven.maitre@gmail.com>
2025-01-13 11:48:36 +0100

    Création de la connexion google grâce à AuthJs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "09cdb0a6fef9b236"; }
+/******/ 		__webpack_require__.h = function() { return "1052338736218de4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -8,8 +8,6 @@ import { ThemeContext } from "@/context/ThemeContext";
 const ThemeToggle = () => {
   const { toogle, theme } = useContext(ThemeContext);
 
-  console.log(theme);
-
   return (
     <div
       className={styles.container}
